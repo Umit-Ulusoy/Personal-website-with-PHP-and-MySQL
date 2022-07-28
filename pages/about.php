73867e93cce654b7c2ac832976325325160d2b45
Umit-Ulusoy <88715653+Umit-Ulusoy@users.noreply.github.com>
2022-07-28 19:54:41 +0300

    'about.php' file is done

--- pages/about.php
+++ pages/about.php
@@ -1,1 +1,9 @@
-<?php echo "<div role='alert'>Hi dude, let's learn who I am!</div>"; ?>
+<div>
+ <h1>Who am I?</h1>
+ <p>Let's learn ho I am.</p>
+ <h2>Education</h2>
+ A university I do not know - 2054
+ <h2>Certificates</h2>
+ None of Developments - 2016 <br />
+ A1.3 English - 2022
+</div>
