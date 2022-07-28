a9b5fc3468431a6fa591235e830f02b91e4fd650
Umit-Ulusoy <88715653+Umit-Ulusoy@users.noreply.github.com>
2022-07-28 15:12:04 +0300

    e-mail form is added to the 'contact.php' file

--- pages/contact.php
+++ pages/contact.php
@@ -1,1 +1,9 @@
-<?php echo "<div role='alert'>Ow, I guess you wanna text someting to me</div>"; ?>
+<div>
+ <h1>tell Me What You Want</h1>
+ <p>Send me e-mail, which includes your demands</p>
+ <input type="text" autofocus required name="firstName" placeholder="Enter your first name" /> <br />
+ <input type="text" required name="lastName" placeholder="Enter your last name" /> <br />
+ <input type="text" required name="subject" placeholder="What is the subject?" /> <br />
+ <textarea required name="message" placeholder="Enter your message"></textarea><br />
+ <button type="submit" name="submit">Send The Mail</button>
+</div>
