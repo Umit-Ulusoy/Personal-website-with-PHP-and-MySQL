643b58257262cf8a73968ae4bcde105dcab20eea
Umit-Ulusoy <88715653+Umit-Ulusoy@users.noreply.github.com>
2022-07-29 11:49:20 +0300

    'index.php' file for admin is done

---
+++ admin/login.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="tr-TR">
+<head>
+ <meta charset="UTF-8">
+ <meta http-equiv="X-UA-Compatible" content="IE=edge">
+ <meta name="viewport" content="width=device-width, initial-scale=1.0">
+ <title>O Buu</title>
+</head>
+<body>
+
+<div>
+ <h1>Let's log in the panel</h1>
+ <form action="" method="POST">
+  <input type="text" required name="username" placeholder="Enter your username" /> <br />
+  <input type="password" required name="password" placeholder="Enter your password" /> <br />
+  <button type="submit" name="submit">Log In</button>
+</form>
+</div>
+
+</body>
+</html>
