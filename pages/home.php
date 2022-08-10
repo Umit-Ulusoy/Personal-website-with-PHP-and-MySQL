b930adbd36a55a2028acd3aee8f1f057e5d7cf0f
Umit-Ulusoy <88715653+Umit-Ulusoy@users.noreply.github.com>
2022-08-10 20:29:51 +0300

    'home.php' file for visiters is done

--- pages/home.php
+++ pages/home.php
@@ -1,1 +1,16 @@
-<?php echo "<div role='alert'>Obaa, elcome to the homepage</div>"; ?>
+<?php
+
+require_once("./classes/Home.php");
+
+$homeClass = new Home($db->conn);
+$home = $homeClass->getHome()->fetch(PDO::FETCH_OBJ);
+
+if($homeClass->getHome()->rowCount() > 0)
+{
+ echo "<h1>$home->Headline</h1>";
+ echo "<pre>";
+ echo $home->Subline;
+ echo "</pre>";
+}else header("Location: pages/404.php");
+
+?>
