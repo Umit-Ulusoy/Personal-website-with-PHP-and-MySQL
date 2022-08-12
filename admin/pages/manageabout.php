d2ecb077a227d7c0e0cbfeaa3ec5226b9c5c02dc
Umit-Ulusoy <88715653+Umit-Ulusoy@users.noreply.github.com>
2022-08-12 13:11:46 +0300

    'manageabout.php' file is done
    'manageabout.php' file is set up and connected to the database

--- admin/pages/manageabout.php
+++ admin/pages/manageabout.php
@@ -1,1 +1,66 @@
-<h2>About</h2>
+<?php
+
+require_once("../classes/About.php");
+
+$about = new About($db->conn);
+
+$getAbout = $about->getAbout()->fetch(PDO::FETCH_OBJ);
+$getEducation = $about->getEducations();
+$getCertificate = $about->getCertificates();
+
+?>
+
+<div>
+ <form action="?page=4" method="POST">
+  <input type="text" required name="headline" placeholder="Enter the headline" value="<?php echo $getAbout->Headline; ?>" /><br />
+  <textarea name="content" placeholder="Enter the content"><?php echo $getAbout->Content; ?></textarea>
+  <button type="submit" name="change">Save Changes</button>
+  <input type="reset" />
+</form>
+</div>
+<div>
+ <h2>Manage Educations</h2>
+ <?php
+
+ if($getEducation->rowCount() > 0)
+ {
+while( $education = $getEducation->fetch(PDO::FETCH_OBJ))
+{?>
+<div>
+ <form action="?page=4" method="POST">
+  <input type="text" required name="education" placeholder="Enter the education" value="<?php echo $education->Education; ?>" /> - 
+  <input type="text" required name="graduationDate" placeholder="Enter the graduation date" value="<?php echo $education->GraduationDate; ?>" /> 
+  <input type="hidden" name="id" value="<?php echo $education->Id; ?>" />
+  <button type="submit" name="editEducation">Edit</button>
+  <button type="submit" name="deleteEducation">Delete</button>
+  <button type="reset">Reset</button>
+</form>
+</div>
+
+<?php }
+ }else echo "<div role='alert'>There is no any education info</div>";
+ ?>
+ </div>
+ <div>
+ <h2>Manage Certificates</h2>
+ <?php
+
+ if($getCertificate->rowCount() > 0)
+ {
+while( $certificate = $getCertificate->fetch(PDO::FETCH_OBJ))
+{?>
+<div>
+ <form action="?page=4" method="POST">
+  <input type="text" required name="certificate" placeholder="Enter the certificate" value="<?php echo $certificate->Certificate; ?>" /> - 
+  <input type="text" required name="issuedDate" placeholder="Enter the issued date" value="<?php echo $certificate->IssuedDate; ?>" /> 
+  <input type="hidden" name="id" value="<?php echo $certificate->Id; ?>" />
+  <button type="submit" name="editCertificate">Edit</button>
+  <button type="submit" name="deleteCertificate">Delete</button>
+  <button type="reset">Reset</button>
+</form>
+</div>
+
+<?php }
+ }else echo "<div role='alert'>There is no any certificate info</div>";
+ ?>
+ </div>
--- pages/about.php
+++ pages/about.php
@@ -3,11 +3,14 @@
 require_once("./classes/About.php");
 
 $about = new About($db->conn);
+$aboutFields = $about->getAbout()->fetch(PDO::FETCH_OBJ);
 
 ?>
 <div>
- <h1>Who am I?</h1>
- <p>Let's learn ho I am.</p>
+ <h1><?php echo $aboutFields->Headline; ?></h1>
+ <pre>
+  <?php echo $aboutFields->Content; ?>
+ </pre>
  <h2>Education</h2>
 <?php
 
